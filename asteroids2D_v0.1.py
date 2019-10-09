be8f65657eaa124a72c085462676bbb4e81631bc
Jeremy VanSchalkwyk <JeremyVS@gmail.com>
2019-10-09 19:16:04 -0400

    Added LCD instantiation, and hello world test

--- asteroids2D_v0.1.py
+++ asteroids2D_v0.1.py
@@ -1,4 +1,4 @@
-# imports and lcd_<pins> are copied from adafruit guide
+# all of this is copied from adafruit guide
 # from: https://learn.adafruit.com/character-lcds/python-circuitpython
 
 import board
@@ -10,3 +10,18 @@ lcd_d6 = digitalio.DigitalInOut(board.D11)
 lcd_d5 = digitalio.DigitalInOut(board.D10)
 lcd_d4 = digitalio.DigitalInOut(board.D9)
 
+lcd_columns = 16
+lcd_rows = 2
+
+import adafruit_character_lcd.character_lcd as characterlcd
+lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
+
+while True:
+    # gratuitous hello world test! ...but tweaked for LCD display testing
+    lcd.message = 'Hello\nCircuitPython!'
+    for i in range(0:15))  
+        sleep(1)
+        lcd.move_left())
+    for i in range(0:15)
+        sleep(1)
+        lcd.move_right()
