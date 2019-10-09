def1578318477951273371f52b97962e3fc508b8
Jeremy VanSchalkwyk <JeremyVS@gmail.com>
2019-10-09 18:53:46 -0400

    Added initial a2D python file

---
+++ asteroids2D_v0.1.py
@@ -0,0 +1,12 @@
+# imports and lcd_<pins> are copied from adafruit guide
+# from: https://learn.adafruit.com/character-lcds/python-circuitpython
+
+import board
+import digitalio
+lcd_rs = digitalio.DigitalInOut(board.D7)
+lcd_en = digitalio.DigitalInOut(board.D8)
+lcd_d7 = digitalio.DigitalInOut(board.D12)
+lcd_d6 = digitalio.DigitalInOut(board.D11)
+lcd_d5 = digitalio.DigitalInOut(board.D10)
+lcd_d4 = digitalio.DigitalInOut(board.D9)
+
